56cc844922aa8738fe7bef32125d8925c1315839
comfortyelfaare <110114807+comfortyelfaare@users.noreply.github.com>
2022-08-09 10:52:11 -0700

    commit 2

---
+++ main.js
@@ -0,0 +1,21 @@
+//logical operators
+if (3==3 && 4==4){
+    document.write("the condition is true.")
+}
+if(3==3 && 4==6){
+document.write("the statement is flase and will not printedbe")
+}
+const age =21;
+ if(age>18){
+    alert("old enough to register");
+ }else{
+    alert("user is not old en0ugh to enter");
+ }   
+const x =10;
+if(x<10){
+    document.getElementById("demo2").innerHTML="x is less than 10";
+}else if(x>10){
+    document.getElementById("demo2").innerHTML="X is greater than 10";
+}else{
+    document.getElementsById("demo2").innerHTML="X is the same as 10";
+}
